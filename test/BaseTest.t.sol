92f2761f3614530418ad57bbca01b778c4f4f1e8
carrotsmuggler <smugglercarrot@gmail.com>
2024-07-13 15:37:52 +0530

    Maintainance

--- test/BaseTest.t.sol
+++ test/BaseTest.t.sol
@@ -9,7 +9,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // Tapioca
 import {TapToken, ITapToken} from "contracts/tokens/TapToken.sol";
-import {TwTAP, TWAMLPool, TWAML} from "contracts/twTAP.sol";
+import {TwTAP, TWAMLPool, TWAML} from "contracts/twTAP2.sol";
 import {MockPearlmit} from "contracts/mocks/MockPearlmit.sol";
 import {MockCluster} from "contracts/mocks/MockCluster.sol";
 import {IPearlmit} from "contracts/interfaces/IPearlmit.sol";
@@ -23,20 +23,22 @@ import {StringUtilsLib} from "string-utils-lib/StringUtilsLib.sol";
 contract TapTokenTest is Test, TWAML {
     using StringUtilsLib for string;
 
-    uint32 aEid = 1;
+    // Simulations parameters
+    uint256 case_num = 3;
+    uint256 version_num = 2;
+
+    // Global variables
     uint256 baseTime;
-
     TapToken tapToken;
+    TwTAP twTap;
 
-    address public userA = address(1001);
-    uint256 public initialBalance = 100 ether;
+    // Constants
     uint256 EPOCH_DURATION = 1 weeks;
     uint256 OPERATIONS_LIMIT = 1000;
 
     /**
      * DEPLOY setup addresses
      */
-    TwTAP twTap;
     address __endpoint;
     address __contributors = address(0x30);
     address __earlySupporters = address(0x31);
@@ -59,11 +61,8 @@ contract TapTokenTest is Test, TWAML {
      * @dev Setup the OApps by deploying them and setting up the endpoints.
      */
     function setUp() public {
-        vm.deal(userA, 1000 ether);
-        vm.label(userA, "userA");
-
         pearlmit = new MockPearlmit("Pearlmit", "1", address(this), 0);
-        cluster = new MockCluster(aEid, __owner);
+        cluster = new MockCluster();
 
         tapToken = new TapToken(
             ITapToken.TapTokenConstructorData(
@@ -91,25 +90,8 @@ contract TapTokenTest is Test, TWAML {
         twTap.setCluster(ICluster(address(cluster)));
         vm.label(address(twTap), "twTAP");
 
-        vm.prank(userA);
-        tapToken.approve(address(pearlmit), type(uint256).max);
-
         // Create and distribute a reward token?
     }
-
-    // function testInitial() external {
-    //     console.log("----Initial----");
-    //     _printTWAMLParams();
-
-    //     deal(address(tapToken), userA, 1e6 ether);
-
-    //     vm.prank(userA);
-    //     twTap.participate(userA, 2000 ether, 21 days);
-
-    //     console.log("----1 participant----");
-    //     _printTWAMLParams();
-    //     _printTWAMLParams_CSV(string(""));
-    // }
 
     struct DataDump {
         uint256 timestamp;
@@ -119,127 +101,191 @@ contract TapTokenTest is Test, TWAML {
         uint256 cumulative;
     }
 
-    function testExecute() external {
-        uint256 case_num = 1;
-        uint256 version_num = 1;
+    struct InputStruct {
+        uint256 timestamp;
+        uint256 operation;
+        address user;
+        uint256 amount;
+        uint256 duration;
+    }
+
+    enum TEST {
+        BASE,
+        FASTEST
+    }
+
+    function testRunSimulation() external {
         string memory ifname = string(abi.encodePacked("./results/inputfiles/test", vm.toString(case_num), ".csv"));
-        string memory ofname = string(
-            abi.encodePacked("./results/outputs/out", vm.toString(case_num), "_v", vm.toString(version_num), ".csv")
-        );
-        string memory line;
+        string memory line = vm.readLine(ifname);
+        TEST testMode;
+        if (line.isEqual("BASE")) {
+            testMode = TEST.BASE;
+        } else if (line.isEqual("FASTEST")) {
+            testMode = TEST.FASTEST;
+        }
+
+        string memory ofname1;
+        string memory ofname2;
+        if (testMode == TEST.BASE) {
+            ofname1 = string(
+                abi.encodePacked("./results/outputs/out", vm.toString(case_num), "_v", vm.toString(version_num), ".csv")
+            );
+        } else if (testMode == TEST.FASTEST) {
+            ofname1 = string(
+                abi.encodePacked(
+                    "./results/outputs/out", vm.toString(case_num), "_v", vm.toString(version_num), "_TWAML.csv"
+                )
+            );
+            ofname2 = string(
+                abi.encodePacked(
+                    "./results/outputs/out", vm.toString(case_num), "_v", vm.toString(version_num), "_Deposits.csv"
+                )
+            );
+        }
+
         uint256 count;
 
         DataDump[] memory dump = new DataDump[](OPERATIONS_LIMIT);
+        DataDump[] memory dumpDeposits = new DataDump[](OPERATIONS_LIMIT);
+        uint256[] memory expirations = new uint256[](OPERATIONS_LIMIT);
+        uint256 expirycounter = 0;
 
         for (;;) {
             line = vm.readLine(ifname);
             if (bytes(line).length == 0) {
                 break;
             }
-            // console.log(line);
-            count++;
-            (uint256 timestamp, uint256 operation, address user, uint256 amount, uint256 duration) = _processLine(line);
-            _executeOp(timestamp, operation, user, amount, duration, count);
-            (uint256 totalParticipants, uint256 averageMagnitude, uint256 totalDeposited, uint256 cumulative) =
-                _getTWAMLParams();
-            dump[count - 1] = DataDump(timestamp, totalParticipants, averageMagnitude, totalDeposited, cumulative);
-        }
 
-        _dumpToFile(ofname, count, dump);
-    }
+            // Read input file
+            InputStruct memory input = _processLineStruct(line);
 
-    function estMaxGrowth() external {
-        uint256 case_num = 1;
-        uint256 version_num = 1;
-        string memory ifname =
-            string(abi.encodePacked("./results/inputfiles/testMaximal", vm.toString(case_num), ".csv"));
-        string memory ofnameTWAML = string(
-            abi.encodePacked(
-                "./results/outputs/out_maxGrowthTWAML", vm.toString(case_num), "_v", vm.toString(version_num), ".csv"
-            )
-        );
-        string memory ofnameDeposits = string(
-            abi.encodePacked(
-                "./results/outputs/out_maxGrowthDeposits", vm.toString(case_num), "_v", vm.toString(version_num), ".csv"
-            )
-        );
-
-        string memory line = vm.readLine(ifname);
-        if (bytes(line).length == 0) {
-            return;
-        }
-
-        (uint256 timeskip, uint256 iterations, address user, uint256 initAmount,) = _processLine(line);
-        DataDump[] memory dump = new DataDump[](iterations);
-        DataDump[] memory dumpDeposits = new DataDump[](iterations);
-        uint256[] memory expirations = new uint256[](iterations);
-        uint256 expirycounter = 0;
-        timeskip = timeskip - baseTime;
-
-        for (uint256 i = 0; i < iterations; i++) {
-            uint256 timestamp = baseTime + i * timeskip;
-            uint256 amount;
-            uint256 duration;
-
-            {
-                (,, uint256 totalDeposited, uint256 cumulative) = _getTWAMLParams();
-
-                (bool success, bytes memory data) =
-                    address(twTap).call(abi.encodeWithSignature("lastEpochCumulative()"));
-
-                if (success) {
-                    cumulative = abi.decode(data, (uint256));
-                    if (cumulative == 0) {
-                        cumulative = EPOCH_DURATION;
-                    }
-                }
-
-                // Find minimum voting amount
-                amount = computeMinWeight(totalDeposited + twTap.VIRTUAL_TOTAL_AMOUNT(), twTap.MIN_WEIGHT_FACTOR());
-                if (initAmount > 0 && i == 0) amount = initAmount;
-                if (amount > 1e25) amount = 1e25;
-
-                // Get max duration
-                uint256 maxMagnitude = cumulative * 4 - 1;
-                uint256 MCsum = maxMagnitude + cumulative;
-                duration = sqrt(MCsum * MCsum - cumulative * cumulative) / EPOCH_DURATION * EPOCH_DURATION;
-                if (duration > twTap.MAX_LOCK_DURATION()) {
-                    duration = twTap.MAX_LOCK_DURATION() / EPOCH_DURATION * EPOCH_DURATION;
-                }
+            // Process values
+            if (testMode == TEST.FASTEST) {
+                (input.amount, input.duration) = _getFastestGrowthParams();
             }
 
             // Check expiry
-            while (timestamp > expirations[expirycounter] && i > 0 && expirycounter < i) {
-                _executeOp(timestamp, 2, address(uint160(expirycounter + 1)), 0, 0, i);
+            while (input.timestamp > expirations[expirycounter] && count > 0 && expirycounter < count) {
+                _executeOp(input.timestamp, 2, address(uint160(expirycounter + 1)), 0, 0, count);
                 expirycounter++;
             }
 
-            {
-                string memory log = string(
-                    abi.encodePacked(
-                        "It:",
-                        vm.toString(i),
-                        ", tstamp:",
-                        vm.toString(timestamp),
-                        ", amt:",
-                        vm.toString(amount / 1e18),
-                        " ETH TAP for weeks:",
-                        vm.toString(duration / EPOCH_DURATION)
-                    )
-                );
-                console.log(log);
-            }
-
-            uint256 tokenId = _executeOp(timestamp, 1, user, amount, duration, i);
+            // Execute operation
+            uint256 tokenId =
+                _executeOp(input.timestamp, input.operation, input.user, input.amount, input.duration, count);
             (, bool voting,,,, uint256 expiry,,,,) = twTap.participants(tokenId);
             expirations[tokenId - 1] = expiry;
-            (uint256 totalParticipants, uint256 averageMagnitude, uint256 totalDeposited, uint256 cumulative) =
-                _getTWAMLParams();
-            dump[i] = DataDump(timestamp, totalParticipants, averageMagnitude, totalDeposited, cumulative);
-            dumpDeposits[i] = DataDump(timestamp, voting ? 1 : 0, 0, amount / 1e18, duration / EPOCH_DURATION);
+
+            // Store results
+            {
+                (uint256 totalParticipants, uint256 averageMagnitude, uint256 totalDeposited, uint256 cumulative) =
+                    _getTWAMLParams();
+                dump[count] = DataDump(input.timestamp, totalParticipants, averageMagnitude, totalDeposited, cumulative);
+                dumpDeposits[count] =
+                    DataDump(input.timestamp, voting ? 1 : 0, 0, input.amount / 1e18, input.duration / EPOCH_DURATION);
+            }
+
+            // Update
+            count++;
         }
-        _dumpToFile(ofnameTWAML, iterations, dump);
-        _dumpToFile(ofnameDeposits, iterations, dumpDeposits);
+
+        // Dump data
+        _dumpToFile(ofname1, count, dump);
+        if (testMode == TEST.FASTEST) {
+            _dumpToFile(ofname2, count, dumpDeposits);
+        }
+    }
+
+    // function testMaxGrowth() external {
+    //     uint256 case_num = 1;
+    //     uint256 version_num = 1;
+    //     string memory ifname =
+    //         string(abi.encodePacked("./results/inputfiles/testMaximal", vm.toString(case_num), ".csv"));
+    //     string memory ofnameTWAML = string(
+    //         abi.encodePacked(
+    //             "./results/outputs/out_maxGrowthTWAML", vm.toString(case_num), "_v", vm.toString(version_num), ".csv"
+    //         )
+    //     );
+    //     string memory ofnameDeposits = string(
+    //         abi.encodePacked(
+    //             "./results/outputs/out_maxGrowthDeposits", vm.toString(case_num), "_v", vm.toString(version_num), ".csv"
+    //         )
+    //     );
+
+    //     string memory line = vm.readLine(ifname);
+    //     if (bytes(line).length == 0) {
+    //         return;
+    //     }
+
+    //     (uint256 timeskip, uint256 iterations, address user,,) = _processLine(line);
+    //     DataDump[] memory dump = new DataDump[](iterations);
+    //     DataDump[] memory dumpDeposits = new DataDump[](iterations);
+    //     uint256[] memory expirations = new uint256[](iterations);
+    //     uint256 expirycounter = 0;
+    //     timeskip = timeskip - baseTime;
+
+    //     for (uint256 i = 0; i < iterations; i++) {
+    //         uint256 timestamp = baseTime + i * timeskip;
+
+    //         (uint256 amount, uint256 duration) = _getFastestGrowthParams();
+
+    //         // Check expiry
+    //         while (timestamp > expirations[expirycounter] && i > 0 && expirycounter < i) {
+    //             _executeOp(timestamp, 2, address(uint160(expirycounter + 1)), 0, 0, i);
+    //             expirycounter++;
+    //         }
+
+    //         {
+    //             string memory log = string(
+    //                 abi.encodePacked(
+    //                     "It:",
+    //                     vm.toString(i),
+    //                     ", tstamp:",
+    //                     vm.toString(timestamp),
+    //                     ", amt:",
+    //                     vm.toString(amount / 1e18),
+    //                     " ETH TAP for weeks:",
+    //                     vm.toString(duration / EPOCH_DURATION)
+    //                 )
+    //             );
+    //             console.log(log);
+    //         }
+
+    //         uint256 tokenId = _executeOp(timestamp, 1, user, amount, duration, i);
+    //         (, bool voting,,,, uint256 expiry,,,,) = twTap.participants(tokenId);
+    //         expirations[tokenId - 1] = expiry;
+    //         (uint256 totalParticipants, uint256 averageMagnitude, uint256 totalDeposited, uint256 cumulative) =
+    //             _getTWAMLParams();
+    //         dump[i] = DataDump(timestamp, totalParticipants, averageMagnitude, totalDeposited, cumulative);
+    //         dumpDeposits[i] = DataDump(timestamp, voting ? 1 : 0, 0, amount / 1e18, duration / EPOCH_DURATION);
+    //     }
+    //     _dumpToFile(ofnameTWAML, iterations, dump);
+    //     _dumpToFile(ofnameDeposits, iterations, dumpDeposits);
+    // }
+
+    function _getFastestGrowthParams() internal returns (uint256 amount, uint256 duration) {
+        uint256 MAX_TAP_LOCK_AMOUNT = 1e25;
+        (,, uint256 totalDeposited, uint256 cumulative) = _getTWAMLParams();
+
+        (bool success, bytes memory data) = address(twTap).call(abi.encodeWithSignature("lastEpochCumulative()"));
+        if (success) {
+            cumulative = abi.decode(data, (uint256));
+            if (cumulative == 0) {
+                cumulative = EPOCH_DURATION;
+            }
+        }
+
+        // Find minimum voting amount
+        amount = computeMinWeight(totalDeposited + twTap.VIRTUAL_TOTAL_AMOUNT(), twTap.MIN_WEIGHT_FACTOR());
+        if (amount > MAX_TAP_LOCK_AMOUNT) amount = MAX_TAP_LOCK_AMOUNT;
+
+        // Get max duration
+        uint256 maxMagnitude = cumulative * 4 - 1;
+        uint256 MCsum = maxMagnitude + cumulative;
+        duration = sqrt(MCsum * MCsum - cumulative * cumulative) / EPOCH_DURATION * EPOCH_DURATION;
+        if (duration > twTap.MAX_LOCK_DURATION()) {
+            duration = twTap.MAX_LOCK_DURATION() / EPOCH_DURATION * EPOCH_DURATION;
+        }
     }
 
     function _dumpToFile(string memory fname, uint256 count, DataDump[] memory dump) internal {
@@ -342,5 +388,15 @@ contract TapTokenTest is Test, TWAML {
         user = address(uint160(newtext[2].parseInt()));
         amount = newtext[3].parseInt() * 1e18;
         duration = newtext[4].parseInt() * EPOCH_DURATION;
+    }
+
+    function _processLineStruct(string memory line) internal returns (InputStruct memory inputData) {
+        string[] memory newtext = line.split(",", 10);
+
+        inputData.timestamp = newtext[0].parseInt() + baseTime;
+        inputData.operation = newtext[1].parseInt();
+        inputData.user = address(uint160(newtext[2].parseInt()));
+        inputData.amount = newtext[3].parseInt() * 1e18;
+        inputData.duration = newtext[4].parseInt() * EPOCH_DURATION;
     }
 }
